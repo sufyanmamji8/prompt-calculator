837973cea5599131d6e3ae2af4676e31e6e78265
sufyanmamji8 <sufyanmamji4@gmail.com>
2024-02-23 21:21:45 +0500

    prompt calculator for practice JS

---
+++ app.js
@@ -0,0 +1,7 @@
+let input = +prompt("Enter first value");
+let secondInput = +prompt("Enter second value");
+
+alert("The addition result of first and second value is " +  ( input + secondInput));
+alert("The subtraction result of first and second value is " +  ( input - secondInput));
+alert("The multiplication result of first and second value is " +  ( input * secondInput));
+alert("The division result of first and second value is " +  ( input / secondInput));
